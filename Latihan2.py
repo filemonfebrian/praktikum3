af152b7f57e99dd6fb1ed78e71dffe754a1f8d4a
filemonfebrian <filemonfebrian1@gmail.com>
2023-10-31 16:21:30 +0700

    first commit

---
+++ Latihan2.py
@@ -0,0 +1,11 @@
+a=input("masukkan nilai a:")
+b=input("masukkan nilai b:")
+print("Variable a=",a)
+print("Variable b=",b)
+print("Hasil penggabungan {0}&{1}={2}".format(a, b, a+b))
+
+# konversi nilai variable
+a=int(a)
+b=int(b)
+print("Hasil penjumlahan {0}+{1}={2}".format(a, b, a+b))
+print("Hasil pembagian {0}/{1}={2:.2f}".format(a, b, a/b))
